288fbcf179fa9701d04f6f2ba4ca164e295b7a07
Elias Segundo Antonio <eliassegundo.segundo@gmail.com>
2021-11-04 21:33:59 -0500

    New version

--- code.js
+++ code.js
@@ -5,9 +5,11 @@ function calculateIMC()
     let height= document.getElementById("campoAltura").value;
     let result= document.getElementById("result");
     let imc= weight/ (height* height);
+    let msg= document.getElementById("msg");
 
     if(imc>0)
     {
+        msg.innerHTML= "Su IMC es:";
         result.innerHTML= imc;
     }else{
         result.innerHTML= "Coloque valores validos";
